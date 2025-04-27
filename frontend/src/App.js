3083d95ff6e005a32e42792a5600534305a0d174
leticia-ribeiro98 <leticia-ribeiro98@hotmail.com>
2025-04-27 16:12:30 -0300

    alterei o deisgn da tela de cadastro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
