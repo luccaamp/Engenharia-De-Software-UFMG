728807703105d6ed4f9f78f5a1e319fe0e80f7c5
Lucas Santos <rafaelcostalucas3@gmail.com>
2025-05-10 17:11:49 -0300

    Adicionando a funcionalidade de alterar notas

--- backend/routes/gradeRoutes.py
+++ backend/routes/gradeRoutes.py
@@ -1,16 +1,23 @@
 from fastapi import APIRouter
-from models.grade import Grade
-from controllers.gradeController import adicionar_grade, remover_grade
+from models.grade import Grade, GradeUpdate
+from controllers.gradeController import adicionar_grade, remover_grade, modificar_grade
 
 router = APIRouter(
     prefix="/grades",
     tags=["grades"]
 )
 
+# Rota para criar uma nota
 @router.post("/")
 async def criar_grade(grade: Grade):
     return await adicionar_grade(grade)
 
-@router.delete("/{disciplina_id}/nota/{nota_id}")
-async def deletar_nota(disciplina_id: str, nota_id: str):
+# Rota para remover uma nota
+@router.delete("/{disciplina_id}/{nota_id}")
+async def excluir_grade(disciplina_id: str, nota_id: str):
     return await remover_grade(disciplina_id, nota_id)
+
+# Rota para modificar uma nota
+@router.put("/{disciplina_id}/{nota_id}")
+async def atualizar_grade(disciplina_id: str, nota_id: str, grade_update: GradeUpdate):
+    return await modificar_grade(disciplina_id, nota_id, grade_update)
